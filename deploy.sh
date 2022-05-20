f8fc5e75f59c326f87cf8532646a87dcd69ebadc
tpcgold <stefan.goldener@theprosperity.company>
2022-05-20 13:35:22 +0200

    update code

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set +e
 gcloud functions deploy gcp-monitoring-to-teams \
     --entry-point=F \
     --memory=128MB \
-    --region=us-central1 \
-    --runtime=go113 \
+    --region=europe-west1 \
+    --runtime=go116 \
     --env-vars-file=.env.yaml \
     --trigger-http \
     --timeout=10s
